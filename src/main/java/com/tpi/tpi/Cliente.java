0cdb68f8caa1e9180f989ccf5c4b55282f8ae779
antopirovani <pirovaniantonella@gmail.com>
2024-08-16 17:53:31 -0300

    Creación de clase cliente (get/ set y herecia de Usuario)

---
+++ src/main/java/com/tpi/tpi/Cliente.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tpi.tpi;
+import java.io.Serializable;
+//import javax.persistence.Basic;
+//import javax.persistence.Entity;
+import java.sql.Date;
+
+/**
+ *
+ * @author anton
+ */
+public class Cliente extends Usuario implements Serializable {
+    
+    String direccion;
+
+    public Cliente(String direccion, int id, String password_, String email, String nombre, Date fecha, int rol) {
+        super(id, password_, email, nombre, fecha, rol);
+        this.direccion = direccion;
+    }
+   
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }    
+    
+}
