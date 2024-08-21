94469df99534055417aebc39b791b84a51da0d47
giulianoh92 <giulianohillebrand@gmail.com>
2024-08-21 00:38:04 -0300

    Codigo de prueba en el Main

--- src/main/java/com/tpi/tpi/Main.java
+++ src/main/java/com/tpi/tpi/Main.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.tpi.tpi;
+import java.sql.Date;
+import java.util.List;
 
 /**
  *
@@ -10,7 +12,26 @@ package com.tpi.tpi;
  */
 public class Main {
     public static void main(String[] args) {
-        Producto prod1 = new Producto(1, "Redmi Note 11", "", 199, 30);
-        System.out.println(prod1.getNombre());
+
+
+        // solamente pruebas de funcionamiento de los objetos
+
+        Cliente cliente = new Cliente("calle 1", 0, "pass", "user@gmail.com", "word", new Date(0));
+        System.out.println(cliente.getNombreUsuario());
+        System.out.println(cliente.getPassword());
+        System.out.println(cliente.getDireccion());
+        System.out.println(cliente.getEmail());
+        System.out.println(cliente.getfechaRegistro());
+
+        Producto prod1 = new Producto(1, "Redmi Note 11 Pro 128GB", "6.7' 5000mAh", 200, 2, new CategoriaDeProducto(0, "Smartphone"));
+        
+        Item item = new Item(1, prod1);
+
+        cliente.addToCarrito(item);
+
+        Carrito carrito = cliente.getCarrito();
+        for (Item curItem : carrito.getItems()) {
+            System.out.println(curItem.getProducto().getNombre());
+        }
     }
 }
