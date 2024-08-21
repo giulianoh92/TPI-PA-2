afd5b847f03736e011223bb0035679d7b3711642
giulianoh92 <giulianohillebrand@gmail.com>
2024-08-21 11:56:50 -0300

    refactorizacion de clase Carrito

--- src/main/java/com/tpi/tpi/Carrito.java
+++ src/main/java/com/tpi/tpi/Carrito.java
@@ -1,23 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tpi.tpi;
+
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author giu
- */
 public class Carrito {
     private List<Item> items = new ArrayList<>();
 
-    Carrito(){
+    public Carrito() {
     }
 
-    //getters y setters
-
+    // Getters y Setters
     public List<Item> getItems() {
         return items;
     }
@@ -25,4 +17,6 @@ public class Carrito {
     public void addItem(Item item) {
         this.items.add(item);
     }
+
+    // Opcional: métodos adicionales como toString(), equals(), hashCode()
 }
