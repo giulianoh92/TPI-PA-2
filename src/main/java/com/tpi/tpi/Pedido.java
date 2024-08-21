885c36e6b7f71899aa6a119494282bb28de76cc5
giulianoh92 <giulianohillebrand@gmail.com>
2024-08-21 00:36:33 -0300

    Boceto de lo que podria ser la clase del pedido/compra

---
+++ src/main/java/com/tpi/tpi/Pedido.java
@@ -0,0 +1,13 @@
+/* 
+package com.tpi.tpi;
+
+import com.tpi.tpi.Cliente;
+
+public class Pedido extends Carrito{ // El pedido tiene todos los atributos de carrito, pero deberia comportarse distinto. no estoy muy seguro de como implementar esta clase...
+    private int id_pedido;
+    private Estado estado;
+    private Pago pago;
+    private Cliente cliente; // cliente pertene a esta clase? como relacionamos cliente con el pedido?
+}
+
+*/
