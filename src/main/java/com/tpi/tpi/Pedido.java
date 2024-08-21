488f419343d21dc2789f05d275cb79a8f4442c61
giulianoh92 <giulianohillebrand@gmail.com>
2024-08-21 11:58:15 -0300

    implementacion de clase Pedidos independiente de Carrito

--- src/main/java/com/tpi/tpi/Pedido.java
+++ src/main/java/com/tpi/tpi/Pedido.java
@@ -1,13 +1,51 @@
-/* 
 package com.tpi.tpi;
 
-import com.tpi.tpi.Cliente;
+import java.util.List;
+import java.util.ArrayList;
 
-public class Pedido extends Carrito{ // El pedido tiene todos los atributos de carrito, pero deberia comportarse distinto. no estoy muy seguro de como implementar esta clase...
+public class Pedido {
+    private List<Item> items;
     private int id_pedido;
     private Estado estado;
     private Pago pago;
-    private Cliente cliente; // cliente pertene a esta clase? como relacionamos cliente con el pedido?
-}
 
-*/
+    public Pedido(int id_pedido, Estado estado, Pago pago) {
+        this.id_pedido = id_pedido;
+        this.estado = estado;
+        this.pago = pago;
+        this.items = new ArrayList<>();
+    }
+
+    // Getters y Setters
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public int getId_pedido() {
+        return id_pedido;
+    }
+
+    public void setId_pedido(int id_pedido) {
+        this.id_pedido = id_pedido;
+    }
+
+    public Pago getPago() {
+        return pago;
+    }
+
+    public void setPago(Pago pago) {
+        this.pago = pago;
+    }
+}
